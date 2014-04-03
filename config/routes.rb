5ee864f71a46dd7562df20627ffd04c750bcc05d
vagrant <vagrant@vagrant.vm>
2014-04-03 17:16:31 +0000

    change index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Hello::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'users#hello'
+ root 'users#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
